6af48cd45f26a2c9a2e1e3622e0475a934e911d4
Apurva Sawant <apurvasawant45@gmail.com>
2022-11-16 11:39:50 +0530

    loading images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import _ from "lodash";
 import "./App.css";
+import Loader from "./loader.svg";
 
 const App = ({ name }) => {
   return `
     <h1> ${_.join(["Hello", name], " ")} </h1>
     <p> Build apps with plain JS but with modern tooling  </p>
+    <img src="${Loader}" alt="loader" />
     `;
 };
 
