0eae84f8f61d6900122859a2640e5b9a80ae1cbb
Apurva Sawant <apurvasawant45@gmail.com>
2022-11-16 12:17:00 +0530

    HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "src", "index.js"),
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   module: {
     rules: [
@@ -41,5 +43,12 @@ module.exports = {
       },
     },
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "vanillaJS app",
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
   mode: "production",
 };
